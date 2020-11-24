f3d15f1e7f9020095353b17371988e9de5b26c01
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-11-24 17:16:40 +0700

    first-e2e

--- e2e/Liking_Restos.spec.js
+++ e2e/Liking_Restos.spec.js
@@ -1,5 +1,17 @@
 Feature('Liking Restos');
+ 
+Before(({ I }) => {
+  I.amOnPage('/#/favourite');
+});
+ 
+Scenario('showing empty liked restos', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Resto tidak ditemukan', '.resto-item__not__found');
+});
 
-Scenario('test something', ({ I }) => {
-
+Scenario('liking one movie', ({ I }) => {
+  I.see('Resto tidak ditemukan', '.resto-item__not__found');
+ 
+  I.amOnPage('/');
+  // … kita akan mengisi uji coba berikutnya …
 });
